97f21f7a7fe3381f970557a13d455a4919a2031b
DarkDhz <blazegammer24@gmail.com>
2021-04-29 17:31:13 +0200

    Added titles to vue

--- app/main.js
+++ app/main.js
@@ -1,11 +1,9 @@
 const app = new Vue({
     el: '#app',
     data: {
-        titulo: 'Hola mundo con Vue',
-        frutas: [
-            {nombre:'Pera', cantidad:10},
-            {nombre:'Manzana', cantidad:0},
-            {nombre:'Platano', cantidad:11}
-        ]
+        title: 'Clinica Sant Gervasi',
+        cite: 'Reservar cita',
+        specialist: 'Consultar especialista',
+        results: 'Consultar resultados'
     }
 })
