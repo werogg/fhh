4284e77b781c80a67c67636a7ac63daab3b32e3a
DarkDhz <blazegammer24@gmail.com>
2021-04-29 17:45:03 +0200

    deleted unused files

--- app/cite/cite.js
+++
@@ -1,9 +0,0 @@
-const app = new Vue({
-    el: '#app',
-    data: {
-        title: 'Clinica Sant Gervasi',
-        cite: 'Reservar cita',
-        specialist: 'Consultar especialista',
-        results: 'Consultar resultados'
-    }
-})
