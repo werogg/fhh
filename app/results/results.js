21596b993ad694a168b4f0c54c84e81e6680fb5a
DarkDhz <blazegammer24@gmail.com>
2021-04-29 17:38:37 +0200

    adde vue to other files

---
+++ app/results/results.js
@@ -0,0 +1,9 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        title: 'Clinica Sant Gervasi',
+        cite: 'Reservar cita',
+        specialist: 'Consultar especialista',
+        results: 'Consultar resultados'
+    }
+})
