1bea85cd8b1d47741f766f3b35c7faf33d7a3615
DarkDhz <blazegammer24@gmail.com>
2021-05-07 10:31:27 +0200

    added treatments dropdown menu

--- app/main.js
+++ app/main.js
@@ -2,8 +2,33 @@ const app = new Vue({
     el: '#app',
     data: {
         title: 'Clinica Sant Gervasi',
+        treatments: 'Tratamientos',
+        tr1: 'Tratamiento para la depresión',
+        tr2: 'Tratamiento para la ansiedad',
+        tr3: 'Tratamiento para ataques de pánico',
+        tr4: 'Tratamiento para TDAH',
+        tr5: 'Tratamiento para el insomnio',
         cite: 'Reservar cita',
         specialist: 'Consultar especialista',
-        results: 'Consultar resultados'
+        results: 'Consultar resultados',
+        copy: '© 2021 Clínica sant gervasi'
     }
 })
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
